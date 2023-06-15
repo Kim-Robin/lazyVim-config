6fa296e86ed27d5d8fd6e1206685f179d237bd5c
login <login@pop-os.localdomain>
2023-06-14 23:17:32 -0400

    Update java lsp

--- lua/plugins/lang/java.lua
+++ lua/plugins/lang/java.lua
@@ -4,15 +4,29 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "java" })
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "java" })
+      end
     end,
   },
 
-  -- correctly setup mason lsp / dap extensions
+  -- correctly setup mason lsp extensions
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "jdtls", "java-test", "java-debug-adapter" })
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "jdtls" })
+      end
+    end,
+  },
+
+  -- correctly setup mason dap extensions
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "javatest", "javadbg" })
+      end
     end,
   },
 
@@ -38,21 +52,25 @@ return {
           local jdtls_path = jdtls_pkg:get_install_path()
           local jdtls_bin = jdtls_path .. "/bin/jdtls"
 
-          local java_test_pkg = mason_registry.get_package("java-test")
-          local java_test_path = java_test_pkg:get_install_path()
-
-          local java_dbg_pkg = mason_registry.get_package("java-debug-adapter")
-          local java_dbg_path = java_dbg_pkg:get_install_path()
-
-          local jar_patterns = {
-            java_dbg_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar",
-            java_test_path .. "/extension/server/*.jar",
-          }
-
           local bundles = {}
-          for _, jar_pattern in ipairs(jar_patterns) do
-            for _, bundle in ipairs(vim.split(vim.fn.glob(jar_pattern), "\n")) do
-              table.insert(bundles, bundle)
+
+          if mason_registry.has_package("java-test") and mason_registry.has_package("java-debug-adapter") then
+            -- jdtls tools configuration for debugging support
+            local java_test_pkg = mason_registry.get_package("java-test")
+            local java_test_path = java_test_pkg:get_install_path()
+
+            local java_dbg_pkg = mason_registry.get_package("java-debug-adapter")
+            local java_dbg_path = java_dbg_pkg:get_install_path()
+
+            local jar_patterns = {
+              java_dbg_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar",
+              java_test_path .. "/extension/server/*.jar",
+            }
+
+            for _, jar_pattern in ipairs(jar_patterns) do
+              for _, bundle in ipairs(vim.split(vim.fn.glob(jar_pattern), "\n")) do
+                table.insert(bundles, bundle)
+              end
             end
           end
 
@@ -82,9 +100,7 @@ return {
 
               local jdtls = require("jdtls")
               local jdtls_config = vim.tbl_deep_extend("force", opts, {
-                on_attach = function(client, buffer)
-                  require("lazyvim.plugins.lsp.format").on_attach(client, buffer)
-                  require("lazyvim.plugins.lsp.keymaps").on_attach(client, buffer)
+                on_attach = require("lazyvim.util").on_attach(function(client, buffer)
                   -- custom keymaps
                   vim.keymap.set("n", "<leader>co", function()
                     require("jdtls").organize_imports()
@@ -95,7 +111,7 @@ return {
                   require("jdtls").setup_dap({ hotcodereplace = "auto" })
                   require("jdtls.dap").setup_dap_main_class_configs()
                   require("jdtls.setup").add_commands()
-                end,
+                end),
                 cmd = {
                   jdtls_bin,
                   "-data",
